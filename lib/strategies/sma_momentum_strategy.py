5280b835424c2d3666757f8e492a5912f25e53ea
Sungwoo Park <s_park@wargaming.net>
2020-05-04 02:00:44 +0200

    [master] sma momentum

--- lib/strategies/sma_momentum_strategy.py
+++ lib/strategies/sma_momentum_strategy.py
@@ -6,28 +6,21 @@ from lib.strategies.strategy_base import StrategyBase
 class SMAMomentumStrategy(StrategyBase):
     """
     SMA Momentum Strategy
-
-    This simply follows the trend of the security.
-
-
-    Usually, used for benchmarking test.
-    It buys a certain amount of security and holds it until the end of the back-test window.
-
-    buy_date: iso-format str, buy order will be created the first market day after the buy_date.
+    Trend is my friend.
     """
     params = (
         ('maperiod', 90),
+        ('rocperiod', 10)
     )
-
+    
     def __init__(self):
-        self.sma = bt.indicators.MovingAverageSimple(self.datas, period=self.params.maperiod)
-        self.roc = bt.ind.ROC(self.sma, period=10)
+        self.sma = bt.indicators.MovingAverageSimple(self.data, period=self.params.maperiod)
+        self.roc = bt.ind.ROC(self.sma, period=self.params.rocperiod)
         self.cu = bt.ind.CrossUp(self.roc, 0)
         self.cd = bt.ind.CrossDown(self.roc, 0.01)
 
     def next(self):
         if not self.position and self.cu[0]:
-            size = int(self.broker.get_cash() * self.cash_margin / self.data)
-            self.order = self.buy(size=size)
+            self.order_target_value(self.data, self.broker.get_cash() * (1.0 - self.params.buy_margin))
         elif self.position and self.cd[0]:
-            self.order = self.sell(size=self.position.size)
+            self.close(self.data)
